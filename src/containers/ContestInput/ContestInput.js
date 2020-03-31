938ce65fd3374a9b1e59914128ff5b13aa34941d
Anuj Bhatt <anujbhatt111@gmail.com>
2020-03-31 19:06:04 +0530

    Updated Contest Input

--- src/containers/ContestInput/ContestInput.js
+++ src/containers/ContestInput/ContestInput.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import ContestPage from "../ContestPage/ContestPage";
 
@@ -8,6 +8,7 @@ import NavbarPage from  "../../components/NavBar";
 
 const ContestInput = props => {
   const [code, setCode] = useState("");
+  const [contests, setContests] = useState();
   const [details, setDetails] = useState({
     code: "",
     name: "",
@@ -15,8 +16,23 @@ const ContestInput = props => {
     problems: []
   });
 
+  const fetchContests = async () => {
+    let res = await fetch(`https://api.codechef.com/contests`, {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${props.token}`
+      }
+    });
+    res = await res.json();
+    res = res.result.data.content;
+    setContests(res.contestList);
+  };
+
+  useEffect(() => {
+    fetchContests();
+  }, []);
+
   const fetchDetails = async () => {
-    console.log("here");
     let res = await fetch(`https://api.codechef.com/contests/${code}`, {
       headers: {
         accept: "application/json",
@@ -33,9 +49,23 @@ const ContestInput = props => {
     });
   };
 
+  const regex = new RegExp(code, "i");
+  const filter =
+    code === ""
+      ? []
+      : contests
+          .filter(contest => regex.test(contest.code))
+          .map(contest => (
+            <div
+              className={styles.contest}
+              onClick={() => setCode(contest.code)}
+            >
+              {contest.code}
+            </div>
+          ))
+          .slice(0, 5);
+
   return details.code == "" ? (
-    <>
-    <NavbarPage />
     <div className={styles.container}>
       <div className={styles.login_container}>
         <h1>Enter Contest Code or Name</h1>
@@ -45,10 +75,10 @@ const ContestInput = props => {
           value={code}
           onChange={e => setCode(e.target.value)}
         />
+        <div className={styles.filter_container}>{filter}</div>
         <button onClick={() => fetchDetails()}>Go!</button>
       </div>
     </div>
-    </>
   ) : (
     <ContestPage details={details} token={props.token} />
   );
